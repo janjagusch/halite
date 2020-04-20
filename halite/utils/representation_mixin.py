0382abf05cb4bad1b75570af56860614cdd6aabf
jjagusch <jan@instaffo.de>
2020-04-20 20:39:18 +0200

    add dataclasses yaml mixin

--- halite/utils/representation_mixin.py
+++ halite/utils/representation_mixin.py
@@ -3,6 +3,9 @@ This module defines a default representation mixin.
 """
 from abc import ABC
 
+from dataclasses_json import DataClassJsonMixin
+import yaml
+
 
 class RepresentationMixin(ABC):
     """
@@ -20,3 +23,22 @@ class RepresentationMixin(ABC):
             [f"{key}={value}" for key, value in self._repr_attrs.items()]
         )
         return f"{self.__class__.__name__}" f"({joined_attrs})"
+
+
+class DataClassYamlMixin(DataClassJsonMixin, ABC):
+    @staticmethod
+    def _read_yaml(file_path):
+        with open(file_path, mode="r") as file_pointer:
+            return yaml.full_load(file_pointer)
+
+    @staticmethod
+    def _write_yaml(obj, file_path):
+        with open(file_path, mode="w") as file_pointer:
+            yaml.dump(obj, file_pointer)
+
+    @classmethod
+    def from_yaml(cls, file_path):
+        return cls.from_dict(cls._read_yaml(file_path))
+
+    def to_yaml(self, file_path):
+        self._write_yaml(self.to_dict(), file_path)
