c462782081c56b21fe42b2034d4492d9f8c76624
H <haakon.robinson@gmail.com>
2020-05-02 22:00:09 +0200

    Added a simple dynamic loader for agents. This way we don't have to worry about bloat in our standalone submission to kaggle.
    I also made a very simple Agent base class, and added two very simple
    default agents (one simply does nothing, while the other is the default
    random agent from Kaggle).

---
+++ halite/agents/depressed_agent/agent.py
@@ -0,0 +1,10 @@
+import random
+from halite.agents.agent import Agent
+
+class DepressedAgent(Agent):
+    """
+    Bot that doesn't feel like doing anything
+    """
+
+    def act(self, observation):
+        return []
